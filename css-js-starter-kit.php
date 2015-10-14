5241f86bfa34dcd635c33f79165da1a4f1760ef3
Alex <alex@adesignr.com>
2015-10-14 09:43:20 +0200

    Small Floating Header fix

--- css-js-starter-kit.php
+++ css-js-starter-kit.php
@@ -16,7 +16,7 @@
  * Plugin Name:       CSS & JS Starter Kit
  * Plugin URI:        adesignr.com
  * Description:       Plugin um nützliche CSS & JS Snippets einzubinden.
- * Version:           1.5.0
+ * Version:           1.5.1
  * Author:            Alex
  * Author URI:        adesignr.com
  * License:           GPL-2.0+

89a76fa3675de9084a8710bbf7030ac026cd39c4
Alex <alex@adesignr.com>
2015-10-20 10:40:20 +0200

    fix that shit

--- css-js-starter-kit.php
+++ css-js-starter-kit.php
@@ -16,7 +16,7 @@
  * Plugin Name:       CSS & JS Starter Kit
  * Plugin URI:        adesignr.com
  * Description:       Plugin um nützliche CSS & JS Snippets einzubinden.
- * Version:           1.7.2
+ * Version:           1.7.3
  * Author:            Alex
  * Author URI:        adesignr.com
  * License:           GPL-2.0+
